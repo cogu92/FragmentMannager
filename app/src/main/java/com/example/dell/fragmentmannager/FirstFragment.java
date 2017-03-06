4b6be1970f30825faa16f614413c26a3dcdc7aff
Nicolas <nicolascogu@hotmail.com>
2017-03-06 13:05:56 +0100

    class 03/06/2017

--- app/src/main/java/com/example/dell/fragmentmannager/FirstFragment.java
+++ app/src/main/java/com/example/dell/fragmentmannager/FirstFragment.java
@@ -1,29 +1,55 @@
 package com.example.dell.fragmentmannager;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements View.OnClickListener {
 
 
+    private static final String TAG =FirstFragment.class.getSimpleName();
+public interface IcomTowActivity{
+    public void getRandomNumberFromFragmentone();
+}
+private IcomTowActivity mInterface;
     public FirstFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context  context) {
+        super.onAttach(context);
+        mInterface=(IcomTowActivity) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        View rootView =inflater.inflate(R.layout.fragment_first, container, false);
+      final Button btnupdatefragmentf = (Button) rootView.findViewById(R.id.Btn_update_fragment_first);
+        btnupdatefragmentf.setOnClickListener(this);
+           return rootView;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        Log.i(FirstFragment.TAG," click ");
+        MainActivity activity = new MainActivity();
+        activity.getRandomNumberFromFragmentone();
+
+    }
 }
--- app/src/main/java/com/example/dell/fragmentmannager/MainActivity.java
+++ app/src/main/java/com/example/dell/fragmentmannager/MainActivity.java
@@ -1,37 +1,90 @@
 package com.example.dell.fragmentmannager;
 
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,FirstFragment.IcomTowActivity{
+
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private FrameLayout mFramecontainer;
     private View mMainLayout;
+    private boolean mTowpane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFramecontainer=(FrameLayout)findViewById(R.id.frame_layout);
-        mMainLayout=(View)findViewById(R.id.activity_main);
-        mMainLayout.setOnClickListener(this);
+        mTowpane=getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT;
+        if(mTowpane)
+        {
+            mFramecontainer = (FrameLayout) findViewById(R.id.frame_layout);
+            mMainLayout = findViewById(R.id.activity_main);
+            mMainLayout.setOnClickListener(this);
+        }
+        else
+        {
+            final Button btnSwap = (Button) findViewById(R.id.btn_Swap);
+            btnSwap.setOnClickListener(this);
+            FragmentManager fragm = getSupportFragmentManager();
+            fragm.beginTransaction().add(R.id.fragment_container_activity_main_land, new FirstFragment(), "First Fragment").commit();
+        }
     }
 
     @Override
     public void onClick(View v) {
-
-        if(v.getId()==R.id.activity_main)
+        if(mTowpane) {
+            if (v.getId() == R.id.activity_main) {
+                Log.v(MainActivity.TAG, "Clicket");
+                FragmentManager fragm = getSupportFragmentManager();
+                fragm.beginTransaction().add(R.id.frame_layout, new FirstFragment(), "FirstFragment").commit();
+            }
+        }
+        else  if (v.getId() == R.id.btn_Swap)
         {
-            Log.v(MainActivity.TAG,"Clicket");
+            FragmentManager fm = getSupportFragmentManager();
+        boolean isFirstFragment=  fm.findFragmentByTag("First Fragment")!=null;
 
-            FragmentManager fragm =getSupportFragmentManager();
-            fragm.beginTransaction().add(R.id.frame_layout, new FirstFragment(),"FirstFragment").commit();
-         }
+            LinearLayout mlinearlayout =(LinearLayout)findViewById(R.id.Linear_content_button);
+int color;
+            Fragment fragmet;
+            String fragmentTag;
+            if(isFirstFragment)
+            {fragmet=new SecontFragment();
+            fragmentTag="Secont Fragment";
+               color=R.color.colorAccent;
+            }
+            else
+            {fragmet=new FirstFragment();
+                fragmentTag="First Fragment";
+                color=R.color.colorPrimaryDark;
+
+            }
+            fm.beginTransaction()
+                    .replace(R.id.fragment_container_activity_main_land,fragmet,fragmentTag).commit();
+            mlinearlayout.setBackgroundColor(ContextCompat.getColor(this,color));
+
+        }
+
+
+    }
+
+    @Override
+    public void getRandomNumberFromFragmentone() {
+        double randomnumber=Math.random();
+        Log.e(MainActivity.TAG,"Random"+randomnumber);
+
+      //  Fragment fragment =findViewById(R.id.Fragment_secont_main);
 
     }
 }
